05f339b4fa6a4faa310fe77490eb26d6394c8c61
Skythel <14922694+Skythel@users.noreply.github.com>
2020-03-30 17:54:56 +0800

    sketches...and stuff

---
+++ statastrophe/foes.js
@@ -0,0 +1,47 @@
+mobs = [
+        {
+            "name": "Homework",
+            "image": "",
+            "level": 1,
+            "hp": 10,
+            "atk": 2,
+            "def": 1,
+            "int": 5
+        },
+        {
+            "name": "Household Chores",
+            "image": "",
+            "level": 1,
+            "hp": 15,
+            "atk": 1,
+            "def": 3,
+            "int": 2
+        },
+        {
+            "name": "Bad Influence",
+            "image": "",
+            "level": 2,
+            "hp": 8,
+            "atk": 5,
+            "def": 3,
+            "int": 3
+        },
+        {
+            "name": "Dragon",
+            "image": "",
+            "level": 2,
+            "hp": 15,
+            "atk": 10,
+            "def": 3,
+            "int": 2
+        },
+        {
+            "name": "Angry Teacher",
+            "image": "",
+            "level": 3,
+            "hp": 20,
+            "atk": 4,
+            "def": 3,
+            "int": 10
+        }
+    ]
